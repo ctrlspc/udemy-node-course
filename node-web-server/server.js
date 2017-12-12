f6640af7d7476e65dc5f9258ed8efc308b5423ce
Jason <ctrlspc@gmail.com>
2017-12-12 20:44:27 +0000

    completed lesson 43

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,21 +1,29 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
+
+app.set('view engine', 'hbs');
+
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello! Express</h1>');
 
-  res.send({
-    name:'Jason',
-    likes:['node', 'express']
+  res.render('home.hbs', {
+    pageTitle:'Home',
+    welcomeMessage:'Welcome to the homepage!',
+    copyright: new Date().getFullYear()
   });
 });
 
 
 app.get('/about',(req,res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle:'About page',
+    copyright: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req,res) => {
