b13a5bcf09a90816187f5ef0db8820c0fb695d16
Jason <ctrlspc@gmail.com>
2017-12-12 21:00:19 +0000

    completed lesson 44

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,17 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
 
 
 app.use(express.static(__dirname + '/public'));
@@ -13,16 +23,14 @@ app.get('/', (req, res) => {
 
   res.render('home.hbs', {
     pageTitle:'Home',
-    welcomeMessage:'Welcome to the homepage!',
-    copyright: new Date().getFullYear()
+    welcomeMessage:'Welcome to the homepage!'
   });
 });
 
 
 app.get('/about',(req,res) => {
   res.render('about.hbs', {
-    pageTitle:'About page',
-    copyright: new Date().getFullYear()
+    pageTitle:'About page'
   });
 });
 
