63c6e133c3a99df22ba92bfda322fd52e5fed48a
Jason <ctrlspc@gmail.com>
2017-12-14 20:23:13 +0000

    Completed lesson 45

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+//register middleware for logging requests
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log("Unable to append to server log");
+    }
+  });
+
+  next();
+})
+
+app.use((req,res,next) => {
+  res.render('maintenance.hbs');
+})
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
